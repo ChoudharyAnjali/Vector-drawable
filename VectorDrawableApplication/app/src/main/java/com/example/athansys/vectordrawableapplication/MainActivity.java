bded9cddc9cd4286e5a4c79364f7def9dd8c5bd9
Mattia Pagini <matpag@users.noreply.github.com>
2017-06-21 23:52:38 +0200

    Removed TextView property

--- VectorDrawableApplication/app/src/main/java/com/example/athansys/vectordrawableapplication/MainActivity.java
+++ VectorDrawableApplication/app/src/main/java/com/example/athansys/vectordrawableapplication/MainActivity.java
@@ -9,9 +9,6 @@ import android.support.v7.content.res.AppCompatResources;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView mTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
